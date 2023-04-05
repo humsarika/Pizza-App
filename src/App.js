0e54acc9d5b59d8cd97dd6a002b0e3032e69e3e4
Sarika Kushwaha <sarika.vsa01@gmail.com>
2023-04-06 01:04:25 +0530

    Created basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68');
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data.map(({ img_url, name, description, rating, price, isVeg }) => {
+        return (
+          <div key={name}>
+            <img src={img_url} alt={name} />
+            <h2>{name}</h2>
+            <p>{description}</p>
+            <div>{rating} stars</div>
+            <div>{price}</div>
+            <div>{isVeg === true ? "🥦" : "🍗"}</div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
---
+++ src/section1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div>
+      bvhabvkb
+    </div>
+  )
+}
+
+export default Section1
